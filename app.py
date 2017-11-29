5cdbf4ab14fd9c4af00e0830700fc940ba0432d9
Allen0835690 <allen89028@gmail.com>
2017-11-29 21:07:55 +0800

    'update'

--- app.py
+++ app.py
@@ -38,9 +38,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    reply = 'hi'
+    t = event.message.textif t == '嘉義'
+        reply = '雞肉飯...'
+
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text='hi'))
 
 
 if __name__ == "__main__":
